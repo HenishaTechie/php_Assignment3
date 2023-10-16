bd02a85cebef165042bd5a1414f0d1fa245cbe0c
HenishaTechie <henisha777parmar@gmail.com>
2023-10-16 10:46:43 +0530

    Q4 done

--- Q4.php
+++ Q4.php
@@ -1,15 +1,11 @@
 <?php
 
-// Directory path
 $directory = 'D:\xampp\htdocs\clg';
 
-// Get the list of files in the directory
 $files = scandir($directory);
 
-// Remove "." and ".." from the list
 $files = array_diff($files, array('.', '..'));
 
-// Display the list of files
 echo '<ol>';
 foreach ($files as $file) {
     echo '<li>' . $file . '</li>';
